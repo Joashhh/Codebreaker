366667c647de1deb8efad32c32429ee9cdf3d7a0
Joashhh <129048031+Joashhh@users.noreply.github.com>
2024-12-08 17:17:39 +0800

    Add files via upload

---
+++ exam.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	if(a==1){
+		cout << "Hello World";
+	}else{
+		long long x, y;
+		cin >> x >> y;
+		cout << x*y;
+	}
+}

519bdcdf54f5bfff420f7cf52d27e52f19c9dc17
Joashhh <129048031+Joashhh@users.noreply.github.com>
2024-12-08 17:18:59 +0800

    Add files via upload

---
+++ 3nplus1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	while(a != 1){
+		cout << a << " ";
+		if(a%2 == 0){
+			a = a/2;
+		}else{
+			a = 3*a+1;
+		}
+	}
+	cout << a << " ";
+}

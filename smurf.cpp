9c21a0e3ab70e8e79df73695a5dea0accbb35202
Joashhh <129048031+Joashhh@users.noreply.github.com>
2025-11-14 16:15:37 +0800

    Update smurf.cpp

--- smurf.cpp
+++ smurf.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-    int N;
-    int J;
-    cin >> N;
-    vector<int> smurfs;
-    for (int i = 0; i < N; ++i) {
-        int smurf;
-        cin >> smurf;
-        if (i % 2 == 0) {
-			smurfs.push_back(smurf);
-        } else {
-			smurfs.insert(smurfs.begin(), smurf);
-        }
-        J = i;
-    }
-    
-    if(J % 2 == 0){
-        reverse(smurfs.begin(), smurfs.end());
-    }
-    
-    for (int i = 0; i < N; ++i) {
-        cout << smurfs[i] << " ";
-    }
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int N;
+	cin >> N;
+	vector<int> arr(N);
+	vector<int> out;
+	for(int i=0; i<N; i++){
+		cin >> arr[i];
+	}
+	for(int i=N-1; i>=0; i-=2){
+		out.push_back(arr[i]);
+	}
+	if(N%2 == 0){
+		for(int i=0; i<N; i+=2){
+			out.push_back(arr[i]);
+		}
+	}else{
+		for(int i=1; i<N; i+=2){
+			out.push_back(arr[i]);
+		}
+	}
+	for(int i : out){
+		cout << i << " ";
+	}
 }

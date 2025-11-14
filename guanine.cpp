a73ef989642a4c8c0875f1588b26cdf6fe3e27d2
Joashhh <129048031+Joashhh@users.noreply.github.com>
2025-11-14 16:08:09 +0800

    Update guanine.cpp

--- guanine.cpp
+++ guanine.cpp
@@ -1,53 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int n;
-    cin >> n;
-    deque<char> seq;
-    
-    while (n--) {
-        string s;
-        cin >> s;
-        
-        if (s == "ADD_BACK") {
-            char a;
-            cin >> a;
-            seq.push_back(a);
-        }
-        
-        if (s == "ADD_FRONT") {
-            char a;
-            cin >> a;
-            seq.push_front(a);
-        }
-        
-        if (s == "SNIP_FRONT") {
-            int a;
-            cin >> a;
-            if (a <= seq.size()) {
-                seq.erase(seq.begin(), seq.begin() + a + 1);
-            }
-        }
-        
-        if (s == "SNIP_BACK") {
-            int a;
-            cin >> a;
-            if (a <= seq.size()) {
-                seq.erase(seq.begin() + a + 1, seq.end());
-            }
-        }
-        
-        if (s == "QUERY") {
-            int a;
-            cin >> a;
-            if (a >= 0 && a < seq.size()) {
-                cout << seq[a] << endl;
-            }
-        }
-    }
-    return 0;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	deque<char> seq;
+	while(n--){
+		string s;
+		cin >> s;
+		if(s == "ADD_BACK"){
+			char a;
+			cin >> a;
+			seq.push_back(a);
+		}
+		if(s == "ADD_FRONT"){
+			char a;
+			cin >> a;
+			seq.push_front(a);
+		}
+		if(s == "SNIP_FRONT"){
+			int a;
+			cin >> a;
+			seq.erase(seq.begin(), seq.begin()+a+1);
+		}
+		if(s == "SNIP_BACK"){
+			int a;
+			cin >> a;
+			seq.erase(seq.begin()+a+1, seq.end());
+		}
+		if(s == "QUERY"){
+			int a;
+			cin >> a;
+			cout << seq.at(a) << endl;
+		}
+	}
 }

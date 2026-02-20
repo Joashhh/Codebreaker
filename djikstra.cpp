40f7ac26eed7b17a858c59cd3e1867ce16b57bf9
Joashhh <129048031+Joashhh@users.noreply.github.com>
2026-02-20 17:25:58 +0800

    Create djikstra.cpp

---
+++ djikstra.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+const long long INF = 1e18;
+int main() {
+    int V, E;
+    cin >> V >> E;
+    vector<vector<pair<int,int>>> adj(V+1);
+    for(int i=0;i<E;i++){
+        int x, y, w;
+        cin >> x >> y >> w;
+        adj[x].push_back({y,w});
+        adj[y].push_back({x,w});
+    }
+    vector<long long> dist(V+1, INF);
+    dist[1] = 0;
+    priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<>> pq;
+    pq.push({0,1});   
+    while(!pq.empty()){
+        auto [d,u] = pq.top(); pq.pop();
+        if(d > dist[u]){
+			continue;
+		}
+        for(auto [v,w] : adj[u]){
+            if(dist[u] + w < dist[v]){
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+    
+    if(dist[V] == INF){
+		cout << -1;
+	}
+    else{
+		cout << dist[V];
+	}
+}
